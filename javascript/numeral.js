771378f42e9340876176ca75bef99c1849dc0c32
Alihaitham26 <alihaithamahmed26@gmail.com>
2022-11-11 07:39:55 +0200

    enhance code and preformance

--- javascript/numeral.js
+++ javascript/numeral.js
@@ -1,19 +1,19 @@
 "use strict";
 /* this module will use to convert between numeral system: [binary,octal,decimal,hexadecimal] */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toBinary = void 0;
 function toBinary(decimal = 0) {
-    if (typeof decimal !== "number") {
+    //some unusual suits
+    if (typeof decimal !== "number")
         return false;
-    }
-    decimal = decimal - (decimal % 1);
-    let binaryArray = [];
-    if (decimal <= 0) {
+    if (decimal < 1)
         return "0";
+    //make the number int
+    decimal = decimal - (decimal % 1);
+    let binary = "";
+    for (let i = decimal; i > 0; i = (i - (i % 2)) / 2) {
+        binary = (i % 2) + binary;
     }
-    while (decimal > 0) {
-        binaryArray.push(decimal % 2);
-        decimal = (decimal - (decimal % 2)) / 2;
-    }
-    console.log(binaryArray);
-    return binaryArray.reverse().join("");
+    return binary;
 }
-module.exports = { toBinary };
+exports.toBinary = toBinary;
