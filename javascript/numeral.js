006bb49c6938440a73a98561e1755bf2ec4c7b9b
Alihaitham26 <alihaithamahmed26@gmail.com>
2022-11-11 11:15:20 +0200

    write toHex function

--- javascript/numeral.js
+++ javascript/numeral.js
@@ -1,15 +1,17 @@
 "use strict";
-/* this module will use to convert between numeral system: [binary,octal,decimal,hexadecimal] */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.toHex = exports.toOctal = exports.toBinary = void 0;
+/* this module will use to convert between numeral system: [binary,octal,decimal,hexadecimal] */
+const hexNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 function toBinary(decimal = 0) {
-    //some unusual suits
+    //some unusual inputs
     if (typeof decimal !== "number")
         return false;
     if (decimal < 1)
         return "0";
     //make the number int
     decimal = decimal - (decimal % 1);
+    //convert into binary
     let binary = "";
     for (let i = decimal; i > 0; i = (i - (i % 2)) / 2) {
         binary = (i % 2) + binary;
@@ -18,12 +20,14 @@ function toBinary(decimal = 0) {
 }
 exports.toBinary = toBinary;
 function toOctal(decimal = 0) {
+    //some unusual inputs
     if (typeof decimal !== "number")
         return false;
     if (decimal < 1)
         return "0";
     //make the number int
     decimal = decimal - (decimal % 1);
+    //convert into octal
     let octal = "";
     for (let i = decimal; i > 0; i = (i - (i % 8)) / 8) {
         octal = (i % 8) + octal;
@@ -32,6 +36,18 @@ function toOctal(decimal = 0) {
 }
 exports.toOctal = toOctal;
 function toHex(decimal = 0) {
-    return "0";
+    //some unusual inputs
+    if (typeof decimal !== "number")
+        return false;
+    if (decimal < 1)
+        return "0";
+    //make the number int
+    decimal = decimal - (decimal % 1);
+    //convert into hex
+    let hex = "";
+    for (let i = decimal; i > 0; i = (i - (i % 16)) / 16) {
+        hex = hexNums[(i % 16)] + hex;
+    }
+    return hex;
 }
 exports.toHex = toHex;
--- typescript/numeral.ts
+++ typescript/numeral.ts
@@ -1,13 +1,15 @@
 /* this module will use to convert between numeral system: [binary,octal,decimal,hexadecimal] */
+const hexNums=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 
 function toBinary(decimal:number=0):string|boolean{
-  //some unusual suits
+  //some unusual inputs
   if(typeof decimal !== "number") return false
   if(decimal<1) return "0"
 
   //make the number int
   decimal=decimal-(decimal%1)
   
+  //convert into binary
   let binary=""
   for(let i=decimal;i>0;i=(i-(i%2))/2){
     binary=(i%2)+binary
@@ -16,12 +18,14 @@ function toBinary(decimal:number=0):string|boolean{
 }
 
 function toOctal(decimal:number=0):string|boolean{
+  //some unusual inputs
   if(typeof decimal !== "number") return false
   if(decimal<1) return "0"
 
   //make the number int
   decimal=decimal-(decimal%1)
   
+  //convert into octal
   let octal=""
   for(let i=decimal;i>0;i=(i-(i%8))/8){
     octal=(i%8)+octal
@@ -29,7 +33,19 @@ function toOctal(decimal:number=0):string|boolean{
   return octal
 }
 function toHex(decimal:number=0):string|boolean{
-  return "0"
+  //some unusual inputs
+  if(typeof decimal !== "number") return false
+  if(decimal<1) return "0"
+
+  //make the number int
+  decimal=decimal-(decimal%1)
+
+  //convert into hex
+  let hex=""
+  for(let i=decimal;i>0;i=(i-(i%16))/16){
+    hex=hexNums[(i%16)]+hex
+  }
+  return hex
 }
 
 
